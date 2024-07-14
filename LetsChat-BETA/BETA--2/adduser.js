630ca6efbaa20a57a2fdf95bacf1bfdd3ef5e9be
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-07-14 16:47:00 +0530

    Added the LetsChat BETA
    Added the Beta builds of LetsChat.

---
+++ LetsChat-BETA/BETA--2/adduser.js
@@ -0,0 +1,22 @@
+
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function addUser()
+  {
+      user_name = document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+          purpose:"adding user"
+      });
+  }
